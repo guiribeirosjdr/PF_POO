9953f96da30f10393d053d82906bff875af5cf20
guiribeirosjdr <143913904+guiribeirosjdr@users.noreply.github.com>
2024-06-20 10:13:05 -0300

    Update funcionario.py

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,10 +1,12 @@
-class Funcionario:
-    def __init__(self, nome, cargo, horario, email):
-        self.nome = nome  # Nome do funcionário.
+from models.pessoa import Pessoa  # Importa a classe Pessoa do módulo models.pessoa para utilizar como superclasse.
+
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cargo, horario, email):
+        super().__init__(nome, idade)  # Chama o construtor da superclasse Pessoa.
         self.cargo = cargo  # Cargo que o funcionário ocupa.
         self.horario = horario  # Horário de trabalho do funcionário.
         self.email = email  # Email do funcionário.
 
     def exibir_informacoes(self):
         # Retorna uma string formatada com as informações do funcionário.
-        return f"Funcionário: {self.nome}, Cargo: {self.cargo}, Horário: {self.horario}, Email: {self.email}"
+        return f"Funcionário: {self.nome}, Idade: {self.idade}, Cargo: {self.cargo}, Horário: {self.horario}, Email: {self.email}"
